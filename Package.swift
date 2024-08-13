0d53282fb03cb2a99c44ae111aab6b528ea2f846
Jeremy Barger <jeremyjusticebarger@gmail.com>
2024-08-13 00:58:32 -0600

    fixing ambiguity

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SecureTextFieldToggle",
+    name: "SecuredTextField",
     platforms: [
             .iOS(.v15),
             .macOS(.v12)
@@ -12,16 +12,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SecureTextFieldToggle",
-            targets: ["SecureTextFieldToggle"]),
+            name: "SecuredTextField",
+            targets: ["SecuredTextField"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SecureTextFieldToggle"),
+            name: "SecuredTextField"),
         .testTarget(
-            name: "SecureTextFieldToggleTests",
-            dependencies: ["SecureTextFieldToggle"]),
+            name: "SecuredTextFieldTests",
+            dependencies: ["SecuredTextField"]),
     ]
 )
